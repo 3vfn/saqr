7c2f3258f09385e0d748243702dbe8969f1cc15c
3vfn <sindazon@gmail.com>
2026-08-12 23:37:23 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'saqr-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
